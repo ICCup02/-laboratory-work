3c92b02ea8a81c990b18328af981f9643b34009b
ICCup02 <56939890+ICCup02@users.noreply.github.com>
2020-01-16 05:33:06 +0300

    1

---
+++ laboratory 2/code/programm for lab2/programm for lab2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <math.h> 
+#define _USE_MATH_DEFINES
+using std::cin;
+using std::cout;
+int main()
+{
+    double e = 2.71828, a, b, dx, x0, xn, fx, max=-1.7976931348623158e+308,min= 1.7976931348623158e+308;
+    cin >> a >> b >> dx >> x0 >> xn;
+    while (x0 <= a) { 
+        fx = pow(e,x0)/(3+sin(x0));
+        if (max < fx) { max = fx; }
+        if (min > fx) { min = fx; }
+        cout << fx << '\n';
+        x0 =x0+dx;
+    }
+    while ((x0 <b )&&(x0>a)) {
+        fx = log(x0) + x0 * x0;
+        if (min > fx) { min = fx; }
+        if (max < fx) { max = fx; }
+        cout << fx << '\n';
+        x0 = x0 + dx;
+    }
+    while ((x0 >= b)&&(x0<=xn+dx/2)) {
+        fx = 1+sin(-x0);
+        if (min > fx) { min = fx; }
+        if (max < fx) { max = fx; }
+        cout << fx << '\n';
+        x0 = x0 + dx;
+    }
+    cout << "max= " << max << std::endl;
+    cout << "min= " << min;
+    return 0;
+}
