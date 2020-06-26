7e97035ecb702326365d0c8b274287cfd008b36c
ICCup02 <56939890+ICCup02@users.noreply.github.com>
2020-06-26 22:51:28 +0300

    new 31

--- programm/31/31.cpp
+++ programm/31/31.cpp
@@ -28,18 +28,32 @@ std::string map[25] = {
 "#         #########       #",
 "#######E############D######"
 };
+struct Coordinate {
+	int X, Y;
+	Coordinate(int x, int y) {
+		X = x; Y = y;
+	}
+};
+std::vector<Coordinate> Close;
 bool CheckWrongData(int x, int y) {
 	if ((x < 25) && (x >= 0) && (y < 27) && (y >= 0))
 		return true;
 	else return false;
 }
 void Search(int x, int y) {
+
+
 	if (CheckWrongData) {
+		for (int i = 0; i < Close.size(); i++) {
+			if ((x == Close[i].X) && (y == Close[i].Y))
+			return;
+		}
 		if ((map[x][y] != '#') && (map[x][y] != ' ')) {
 			std::cout << map[x][y] << "\n";
 		}
 		else if (map[x][y] == ' ') {
-			map[x][y] = '#';
+			Coordinate temp(x,y);
+			Close.push_back(temp);
 			Search(x - 1, y);
 			Search(x + 1, y);
 			Search(x, y - 1);
